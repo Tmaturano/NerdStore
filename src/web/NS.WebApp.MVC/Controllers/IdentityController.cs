0a4a7cf5c4ec4a0a37c8441c6df1ec15f8e0bee6
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-03-22 12:45:11 -0300

    Add Views for Login and Register new account

--- src/web/NS.WebApp.MVC/Controllers/IdentityController.cs
+++ src/web/NS.WebApp.MVC/Controllers/IdentityController.cs
@@ -1,11 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NS.WebApp.MVC.Models;
 
 namespace NS.WebApp.MVC.Controllers;
 
 public class IdentityController : Controller
 {
-    public IActionResult Index()
+    [HttpGet("new-account")]
+    public IActionResult Register()
     {
         return View();
+    }
+
+    [HttpPost("new-account")]
+    public async Task<IActionResult> Register(UserRegister newUser)
+    {
+        if (!ModelState.IsValid) return View(newUser);
+
+
+        if (false) return View(newUser);
+
+        return RedirectToAction("Index", controllerName: "Home");
+    }
+
+    [HttpGet("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserLogin login)
+    {
+        if (!ModelState.IsValid) return View(login);
+
+
+        if (false) return View(login);
+
+        return RedirectToAction("Index", controllerName: "Home");
+    }
+
+    [HttpGet("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        return RedirectToAction("Index", controllerName: "Home");
     }
 }
