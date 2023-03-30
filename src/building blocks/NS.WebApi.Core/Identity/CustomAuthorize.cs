498745acac9cc637d196df4794b7ea192b3dbd7d
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-03-30 18:51:34 -0300

    Added access validation by using claims

---
+++ src/building blocks/NS.WebApi.Core/Identity/CustomAuthorize.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace NS.WebApi.Core.Identity;
+
+public class CustomAuthorize
+{
+    public static bool ValidateUserClaims(HttpContext context, string claimName, string claimValue)
+    {
+        return context.User.Identity.IsAuthenticated &&
+            context.User.Claims.Any(c => c.Type == claimName && c.Value.Contains(claimValue));
+    }
+}
+
+public class ClaimsAuthorizeAttribute : TypeFilterAttribute
+{
+    public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequirementClaimFilter))
+    {
+        Arguments = new object[] { new Claim(claimName, claimValue) };
+    }
+}
+
+public class RequirementClaimFilter : IAuthorizationFilter
+{
+    private readonly Claim _claim;
+
+    public RequirementClaimFilter(Claim claim) => _claim = claim;
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        if (!context.HttpContext.User.Identity.IsAuthenticated)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+            return;
+        }
+
+        if (!CustomAuthorize.ValidateUserClaims(context.HttpContext, _claim.Type, _claim.Value))
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+        }
+    }
+}
--- src/services/NS.Catalog.API/Controllers/CatalogController.cs
+++ src/services/NS.Catalog.API/Controllers/CatalogController.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NS.Catalog.API.Models;
+using NS.WebApi.Core.Identity;
 
 namespace NS.Catalog.API.Controllers;
 
 [ApiController]
 [Route("api/catalog")]
+[Authorize]
 public class CatalogController : ControllerBase
 {
     private readonly IProductRepository _productRepository;
 
     public CatalogController(IProductRepository productRepository) => _productRepository = productRepository;
 
-    [HttpGet("products")]
+    [AllowAnonymous]
+    [HttpGet("products")]    
     public async Task<IEnumerable<Product>> GetProducts()
         => await _productRepository.GetAllAsync();
 
+    [ClaimsAuthorize("Catalog", "Read")] 
     [HttpGet("products/{id}")]
     public async Task<Product> GetProductDetail(Guid id)
         => await _productRepository.GetAsync(id);
