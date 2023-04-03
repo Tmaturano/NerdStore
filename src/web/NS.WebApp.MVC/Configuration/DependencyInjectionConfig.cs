d58fc17748c082b0bd6c9db372b7a8ccc89941c4
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-04-03 10:52:23 -0300

    Add HttpClient handler to handle the Jwt Token when calling API's that requires authorization

--- src/web/NS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,6 @@
 ﻿using NS.WebApp.MVC.Extensions;
 using NS.WebApp.MVC.Services;
+using NS.WebApp.MVC.Services.Handlers;
 
 namespace NS.WebApp.MVC.Configuration;
 
@@ -7,8 +8,13 @@ public static class DependencyInjectionConfig
 {
     public static void RegisterServices(this IServiceCollection services)
     {
+        services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+                
         services.AddHttpClient<IAuthenticationService, AuthenticationService>();
-        services.AddHttpClient<ICatalogService, CatalogService>();
+        
+        services.AddHttpClient<ICatalogService, CatalogService>()
+            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
+
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<IUser, AspNetUser>();
 
---
+++ src/web/NS.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,29 @@
+﻿using NS.WebApp.MVC.Extensions;
+using System.Net.Http.Headers;
+
+namespace NS.WebApp.MVC.Services.Handlers;
+
+public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+{
+    private readonly IUser _user;
+
+    public HttpClientAuthorizationDelegatingHandler(IUser user)
+    {
+        _user = user;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var authorizationHeader = _user.GetHttpContext().Request.Headers.Authorization;
+        
+        if(!string.IsNullOrEmpty(authorizationHeader) ) 
+            request.Headers.Add("Authorization", new List<string> { authorizationHeader });
+
+        var token = _user.GetToken();
+        
+        if (token is not null)
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        return base.SendAsync(request, cancellationToken);
+    }
+}
