67c3e2f13a498f16226ef5c976bfd70774d9706c
Thiago Maturana de Oliveira <thiagomatu@gmail.com>
2023-06-08 08:37:29 -0300

    Improvements on the basket view

---
+++ src/web/NS.WebApp.MVC/Extensions/BasketViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NS.WebApp.MVC.Models;
+using NS.WebApp.MVC.Services;
+
+namespace NS.WebApp.MVC.Extensions;
+
+public class BasketViewComponent : ViewComponent
+{
+    private readonly IBasketService _basketService;
+
+    public BasketViewComponent(IBasketService basketService) => _basketService = basketService;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View(await _basketService.GetBasketAsync() ?? new BasketViewModel());
+    }
+}
